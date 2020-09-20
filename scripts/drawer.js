2abd88d450b3b18cec267262ab27294a4a4cdd69
mollzilla <eymili@gmail.com>
2020-09-20 19:44:59 -0300

    mobile drawer

--- scripts/drawer.js
+++ scripts/drawer.js
@@ -1,4 +1,14 @@
 document.querySelector("#burger").addEventListener("click", function () {
   let drawer = document.querySelector(".drawer");
+
+  if (this.getAttribute("src")=="./assets/close.svg")
+    this.setAttribute("src", "./assets/burger.svg")
+  else
+    this.setAttribute("src", "./assets/close.svg")
+
+  drawer.classList.toggle("visible")
   
+
+
+
 })
