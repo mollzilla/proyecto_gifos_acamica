da4d23b3390f4a3b1554eddf4866f94318b8eb73
mollzilla <eymili@gmail.com>
2020-09-20 21:25:06 -0300

    overlay ready and simplified search function

--- scripts/search_results.js
+++ scripts/search_results.js
@@ -18,32 +18,35 @@ let lastValue=searchInputValue.value;
 
 async function search(e) {
 
+  /* Two different values for let fetch depending on the need for an offset (view more clicked) */
+
   e.preventDefault();
   let searchResults;
-  console.log(searchInputValue.value)
 
   resultsGrid.style.display="none";
+  let searchFetch;
 
   noResults.style.display="none";
+
+  /*if the id of the first element path of the event clicked is view more, fetch will equal a paginated search with offset */
   if (e.path[0].id==viewMore.id)
   {
-    const searchFetch= await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchInputValue.value}?&api_key=${apiKey}&limit=12&offset=${offset}`);
-    searchResults = await searchFetch.json();
-    appendSearchResults();
-    viewMore.style.display="block";
+    searchFetch= await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchInputValue.value}?&api_key=${apiKey}&limit=12&offset=${offset}`);
   }
   else
   {
     offset=0;
 
-    const searchFetch= await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchInputValue.value}?&api_key=${apiKey}&limit=12`);
-    searchResults = await searchFetch.json();
+    searchFetch= await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchInputValue.value}?&api_key=${apiKey}&limit=12`);
     resultsGrid.innerHTML="";
-    appendSearchResults();
-    viewMore.style.display="block";
   }
+    searchResults = await searchFetch.json();
+    viewMore.style.display="block";
+    appendSearchResults();
    
 
+  /* appending search results to the grid */
+  
   function appendSearchResults() {
     let searchArgument=document.querySelector("#search-argument");
     searchArgument.textContent=searchInputValue.value;
@@ -61,12 +64,57 @@ async function search(e) {
 
       let resultGif=document.createElement("div");
       resultGif.classList.add("result-placeholder");
-      resultGif.setAttribute('id', `result-item-${offset}`)
-      resultGif.style.backgroundImage=`url("${result.images.fixed_width.url}")`
-      resultsGrid.appendChild(resultGif)
+      resultGif.setAttribute('id', `result-item-${offset}`);
+      resultGif.style.backgroundImage=`url("${result.images.fixed_width.url}")`;
+      
+      resultGif.appendChild(createOverlay(result));
+
+      resultsGrid.appendChild(resultGif);
 
       offset++;
     });
+
+    function createOverlay(gifItem) {
+
+      /* adds a child div to a gifItem positioned absolutely that behaves as an overlay, containing both icons and title */
+      let overlay=document.createElement("div");
+      overlay.classList.add("overlay");
+
+      /* Icons */
+      let actionIcons=document.createElement("div");
+      actionIcons.classList.add("action-icons");
+
+      let like=document.createElement("a");
+      let download=document.createElement("a");
+      let expand=document.createElement("a");
+
+      [like, download, expand].forEach(a => {
+        a.classList.add("action-icon");
+        actionIcons.appendChild(a);
+      });
+
+      /* username and title*/
+
+      let username=document.createElement("p");
+      username.classList.add("username");
+      username.textContent=gifItem.username || "Anon";
+
+      let gifTitle=document.createElement("div");
+      gifTitle.classList.add("gif-title");
+
+      let title=document.createElement("p");
+      title.classList.add("title");
+      title.textContent=gifItem.title || "Sin nombre";
+
+      /* Append to the overlayed div */
+
+      gifTitle.appendChild(username);
+      gifTitle.appendChild(title);
+      
+      overlay.appendChild(actionIcons);
+      overlay.appendChild(gifTitle);
+      return overlay;
+    }
  
   }
   suggestion.innerHTML="";
