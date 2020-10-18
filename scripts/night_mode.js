f7f3dbc22799a7f258aca282feb955837e66601b
mollzilla <eymili@gmail.com>
2020-10-18 14:50:41 -0300

    rutas relativas

--- scripts/night_mode.js
+++ scripts/night_mode.js
@@ -13,7 +13,7 @@ function themeToggler(e) {
     document.documentElement.setAttribute("data-theme", "dark");
     themeToggleLinkSm.textContent="Modo Diurno";
     themeToggleLinkLg.textContent="Modo Diurno";
-    allIcons.map(icon => icon.setAttribute('src', `assets/${icon.id}_modo_noc.svg`))
+    allIcons.map(icon => icon.setAttribute('src', `../assets/${icon.id}_modo_noc.svg`))
     console.log(allIcons)
 
     document.querySelector("#burger").setAttribute("src", "../assets/icon_close_modo_noc.svg");
